f0d449c1d9ba8c23221ebf7d20f48fe67cab7604
Yuvraj Singh <58326592+iamyuvraj@users.noreply.github.com>
2024-11-09 17:39:38 +0530

    Update Backup.sh

--- Backup.sh
+++ Backup.sh
@@ -16,34 +16,33 @@ function throw() {
   exit 1
 }
 
-print_info "Please grant termux storage permission on next screen..."
+print_info "Please Grant Storage Permission on Next Screen..."
 termux-setup-storage
 
-print_info "!! Make sure you installed termux from fdroid"
-print_info " and not PlayStore. !!"
+print_info "Make sure you've installed Termux from FDroid"
 
-print_info "Please confirm whether you have installed termux from fdroid or not."
-print_info "Enter 'y' for yes and 'n' for no."
+print_info "Please confirm if installed Termux from FDroid or not."
+print_info "Enter 'y' for Yes and 'n' for No."
 read -n 1 confirmation
 echo -ne "\b"
 if [ "$confirmation" == "y" ]; then
-    print_info "Continuing"
+    print_info "Continuing..."
 else
-    print_info "Don't worry, we will download termux fdroid apk for you."
-    print_info "One moment please"
+    print_info "Don't worry, we will download Termux FDroid .apk file for you."
+    print_info "Loading..."
     curl https://f-droid.org/repo/com.termux_117.apk \
-        -so /sdcard/Download/com.termux_117.apk || throw "Failed to download termux APK, please try again or download it manually from https://f-droid.org/repo/com.termux_117.apk"
-    print_info "APK downloaded and is located at internal storage/Download/com.termux_117.apk"
-    print_info "Please install the APK and run this script again."
+        -so /sdcard/Download/com.termux_117.apk || throw "Failed to Download Termux APK, please try again or download it manually from https://f-droid.org/repo/com.termux_117.apk"
+    print_info "APK Downloaded and is located at internal storage/Download/com.termux_117.apk"
+    print_info "Please Install the APK and Run this Script again."
     exit 0
 fi
 
-print_info "Installing required packages"
-print_info " - Installing updates, you can skip this by pressing 'y' within"
-print_info " - 5 seconds ... (Useful if you have limited data)"
+print_info "Installing Required Packages"
+print_info " - Installing Updates, you can skip this by pressing 'y' within..."
+print_info " - 5 Seconds... (useful if you have limited data)"
 read -n1 -t5 skip_update
 if [[ "$skip_update" =~ [yY] ]]; then
-    print_info "Skipping update, only required packages will be installed"
+    print_info "Skipping Update, only required Packages will be Installed"
 else
     pkg update -y &>/dev/null || err=1
     yes | pkg upgrade &>/dev/null || err=1
@@ -51,19 +50,19 @@ else
 fi
 pkg install tsu zip -y &>/dev/null || err=1
 if [ "$err" == "1" ]; then
-    throw "Failed to install packages, check your internet connection and try again."
+    throw "Failed to Install Packages, check your internet connection and try again."
 fi
 
 testfile=/cache/test.$RANDOM
 print_info "Checking for root access"
-sudo touch $testfile || throw "Root access not granted. Please grant root access and run this script again."
+sudo touch $testfile || throw "Root Access not Granted. Please Grant Root Access and Run this Script again."
 sudo rm -f $testfile
 
-print_info "Making directory"
+print_info "Making Directory"
 tmpdir="tmpdir-$RANDOM"
 mkdir /sdcard/$tmpdir
 
-print_info "Extracting partitions"
+print_info "Extracting Partitions"
 partitions="
 nvram
 nvcfg
@@ -77,14 +76,14 @@ for part in $partitions; do
   sudo dd if="/dev/block/by-name/$part" of="/sdcard/$tmpdir/$part.img" status="none"
 done
 
-print_info "Packing partitions to save space"
-filename="IMEI_backup_$(date +%Y-%m-%d@%H-%M-%S).zip"
+print_info "Packing Partitions to Save Space"
+filename="IMEI_Backup_$(date +%Y-%m-%d@%H-%M-%S).zip"
 # shellcheck disable=SC2164
 cd /sdcard
 zip -qr $filename $tmpdir
 
-print_info "Cleanup"
+print_info "Clean-Up"
 rm -rf $tmpdir
 
-print_info "Done. File is located in internal storage with filename:"
+print_info "Done. File is located in Internal Storage with Filename: "
 print_info "$filename"
